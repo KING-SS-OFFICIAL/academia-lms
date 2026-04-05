d09cfd207b753a2adee88c806ecbd8c0cfa76ecd
KING-SS-OFFICIAL <king.ss.official@github.com>
2026-04-05 07:02:12 +0000

    Add mobile bottom nav to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Specializations from "@/components/home/Specializations";
 import WhyChoose from "@/components/home/WhyChoose";
 import StatsSection from "@/components/home/StatsSection";
 import CTASection from "@/components/home/CTASection";
+import MobileBottomNav from "@/components/dashboard/MobileBottomNav";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChoose />
       <StatsSection />
       <CTASection />
+      <MobileBottomNav />
     </>
   );
 }
